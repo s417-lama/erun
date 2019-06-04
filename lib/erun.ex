18e0482b3bd592f59c5e90d7f725e3a634cfead6
slama <s417.lama@gmail.com>
2019-06-04 20:34:50 +0900

    support string commands by -e option

--- lib/erun.ex
+++ lib/erun.ex
@@ -1,11 +1,19 @@
 defmodule Erun do
-  def main([]) do
-    IO.puts("Usage: ./erun <filename> arg1, arg2, ...")
-    exit({:shutdown, 1})
+  def main(["-e", string | args]) do
+    Agent.start_link(fn -> args end, name: __MODULE__)
+    Code.eval_string(string)
   end
   def main([filename | args]) do
     Agent.start_link(fn -> args end, name: __MODULE__)
     Code.eval_file(filename)
+  end
+  def main(_) do
+    IO.puts(:stderr, """
+      Usage: ./erun [options] [eval_string | file] arg1, arg2, ...
+
+        -e eval_string          Evaluates the given eval_string
+      """)
+    exit({:shutdown, 1})
   end
 
   def args() do
